d036878fcdc3669eee1fcb9a9f5423bde4622e85
Victor Miraldo <v.cacciarimiraldo@gmail.com>
2018-04-20 11:56:20 +0200

    trying out xmobar

--- VMonad.hs
+++ VMonad.hs
@@ -1,14 +1,24 @@
 import XMonad
-import XMonad.Config.Mate
-import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
 
 -- My modifier, Alt_R
 myMod = mod1Mask
 
 -- My config
-myConfig = mateConfig 
-  { modMask = myMod
+myConfig xmproc = def 
+  { modMask            = myMod
   , focusedBorderColor = "#00FFAA"
+  , manageHook         = manageDocks 
+                       <+> myManageHook
+                       <+> manageHook def
+  , logHook            = dynamicLogWithPP $ xmobarPP
+                           { ppOutput = hPutStrLn xmproc
+                           , ppTitle  = xmobarColor "green" "" . shorten 50
+                           }
   } `additionalKeys` myKeys
 
 
@@ -17,7 +27,11 @@ myKeys = [
     ((myMod , xK_q), spawn "cd /home/victor/.xmonad && stack install && xmonad --restart" )
   ]
 
+myManageHook = composeAll
+    [ className =? "Gimp"      --> doFloat
+    , className =? "Vncviewer" --> doFloat
+    ]
 
--- Registering and launching
-main = mateRegister 
-    >> xmonad myConfig -- we need 'xmonad' here because we want CLI argument parsing.
+main = do
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ myConfig xmproc
