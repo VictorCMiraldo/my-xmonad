4a96e6c2c44ccd1a7efa3cc648d6c922da69ce30
Victor Miraldo <v.cacciarimiraldo@gmail.com>
2018-04-21 16:37:03 +0200

    Added riced rofi and some comments in the file

--- VMonad.hs
+++ VMonad.hs
@@ -6,14 +6,63 @@ import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Config.Mate
 import System.IO
 
--- My modifier, Alt_R
+-------------------
+-- * Eye Candy * --
+-------------------
+
+myFocusedColor = "#E9B96E"
+myBgColor      = "#2F343F"
+myTextColor    = "#D3DAE3"
+
+-------------------------
+-- * Custom Commands * --
+-------------------------
+
+myRofiCmd :: String
+myRofiCmd = unwords [ "rofi" 
+          , "-show"     , "run" 
+          , "-lines"    , "7" 
+          , "-matching" , "fuzzy"
+          , "-theme"    , "/home/victor/.xmonad/myrofi.rasi"
+          ]
+
+------------------------
+-- * My Keybindings * --
+------------------------
+
+myKeys = [ 
+  -- It is VERY important to override the restart command.
+    ((myMod , xK_q), spawn "cd /home/victor/.xmonad && stack install && xmonad --restart" )
+  , ((myMod , xK_d), spawn myRofiCmd)
+  ]
+
+-------------------------
+-- * My Manage Hooks * --
+-------------------------
+
+myManageHook = composeAll
+    [ className =? "Gimp"      --> doFloat
+    , className =? "Vncviewer" --> doFloat
+    ]
+----------------------------
+-- * My modifier, Alt_R * --
+----------------------------
+
+-- We later use xmodmap to map Caps_Lock
+-- to Alt_R and live happily ever after.
 myMod = mod1Mask
 
--- My config
+-------------------
+-- * My Config * --
+-------------------
+
+-- We receive a handle as parameter since on the main
+-- function we spawn the xmobar process and need
+-- to do some wiring here.
 myConfig xmproc = mateConfig 
   { modMask            = myMod
   , terminal           = "mate-terminal"
-  , focusedBorderColor = "#729FCF"
+  , focusedBorderColor = myFocusedColor
   , manageHook         =   manageDocks
                        <+> myManageHook
                        <+> manageHook def
@@ -21,24 +70,17 @@ myConfig xmproc = mateConfig
                        $ layoutHook def
   , logHook            = dynamicLogWithPP xmobarPP
                           { ppOutput = hPutStrLn xmproc
-                          , ppTitle  = xmobarColor "#729FCF" "" . shorten 70
+                          , ppTitle  = xmobarColor myFocusedColor "" . shorten 70
                           }
   , handleEventHook    = handleEventHook def
                        <+> docksEventHook
   } `additionalKeys` myKeys
 
-myKeys = [ 
-  -- It is VERY important to override the restart command.
-    ((myMod , xK_q), spawn "cd /home/victor/.xmonad && stack install && xmonad --restart" )
-  , ((myMod , xK_d), mateRun)
-  ]
+----------------------------
+-- * Running everything * --
+----------------------------
 
-myManageHook = composeAll
-    [ className =? "Gimp"      --> doFloat
-    , className =? "Vncviewer" --> doFloat
-    ]
-
-main = do -- mateRegister
+main = do mateRegister
           xmproc <- spawnPipe "xmobar"
           xmonad (myConfig xmproc)
 
